ad5a17736b26a842aa524816442577ef0da89fdb
Artur Swiech <artur.swiech@gmail.com>
2018-07-25 20:00:25 +0200

    Vector implementation

--- homework01/vector.h
+++ homework01/vector.h
@@ -1,5 +1,35 @@
 
 class Vector
 {
-	// TODO
+ public:
+  Vector(double x = 0., double y = 0.)
+    {
+      this->x = x;
+      this->y = y;
+    }
+  
+  bool operator==(Vector other)
+  {
+    return (this->GetX() == other.GetX()) && (this->GetY() == other.GetY());
+  }
+  
+  void Add(Vector other)
+  {
+    this->x += other.GetX();
+    this->y += other.GetY();
+  }
+  
+  double GetX()
+  {
+    return this->x;
+  }
+
+  double GetY()
+  {
+    return this->y;
+  }
+
+ private:
+  double x;
+  double y;
 };

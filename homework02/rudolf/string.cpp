af220e74ed78b4a58cc2f6d720c5ca92a47c069a
Rudolf <croosthuizen@croost.lan>
2018-08-18 22:22:25 +0200

    updating homework02

--- homework02/rudolf/string.cpp
+++ homework02/rudolf/string.cpp
@@ -1,68 +1,76 @@
 #include "string.h"
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <algorithm>
 
 String::String():
-		mString(nullptr),
 		mSize(0),
-		mCapacity(0)
-{};
-String::String(const char* chars,std::size_t capacity):
-		mString(new char[capacity>strlen(chars)?capacity:strlen(chars)]),
+		mCapacity(0),
+		mString(nullptr)
+{}
+
+String::String(const char* chars, std::size_t capacity):
 		mSize(strlen(chars)),
-		mCapacity(capacity>strlen(chars)?capacity:strlen(chars))
+		mCapacity(capacity > mSize ? capacity : mSize),
+		mString(std::make_unique<char[]>( capacity > mSize ? capacity : mSize))
 {
-	std::copy_n(chars,mSize,mString.get());
+	std::copy_n(chars, mSize, mString.get());
 };
+String::String(const char* chars):
+	String(chars, strlen(chars) + 1)
+{};
+
 String String::operator=(String str)
 {
-	std::swap(mString,str.mString);
-	mSize =str.size();
-	mCapacity =str.capacity();
+	std::swap(mString, str.mString);
+	mSize = str.size();
+	mCapacity = str.capacity();
 	return *this;
 }
+const bool String::operator==(const String& str)const
+{	
+	return std::equal(c_str() , c_str() + mSize , str.c_str());
+};
 String& String::operator+(const String& str)
 {	
-	String str2(c_str(),size()+str.size());
-	for(std::size_t i=0;i<str.mSize; i++) 
+	String str2(c_str(), size() + str.size());
+	for(std::size_t i=0; i < str.mSize; i++) 
 	{		
 		str2.push_back(str.mString[i]);
 	}
-	*this=str2;
+	*this = str2;
 	return *this;
 }
 void String::push_back(const char c)
 {
 	if (mSize==mCapacity)
-		{add_capacity();}
+	{
+		add_capacity();
+	}
 	mString[mSize] = c;
 	mSize+=1;
 }
 void String::add_capacity()
 {
-	std::unique_ptr<char[]> newString(new char[mCapacity*2 + 1]);
-	std::copy_n(mString.get(),mSize,newString.get());
+	std::unique_ptr<char[]> newString(std::make_unique<char[]>(mCapacity * 2 + 1));
+	std::copy_n(mString.get() ,mSize, newString.get());
 	mString=std::move(newString);
 }
 
-std::size_t strlen(const char* chars)
-{
-	std::size_t counter(0);
-	while(chars[counter]){++counter;}
-	return counter;
-}
-
-bool operator==(const std::string& str1,const String& str2)
+bool operator==(const std::string& str1, const String& str2)
 {
 	return str1==std::string(str2);
 }
 
-bool operator==(const char* chars,const String& str)
+bool operator==(const char* chars, const String& str)
 {
 	return String(chars)==str;
 };
 
 String operator+(const char* chars, const String& str)
 {
-	return String(chars)+str;
+	return String(chars) + str;
 }
 
 std::ostream& operator<<(std::ostream& os, const String& string)
--- homework02/rudolf/string.h
+++ homework02/rudolf/string.h
@@ -1,51 +1,50 @@
 #pragma once
-#include <iostream>
+#include <iosfwd>
 #include <memory>
-#include <algorithm>
-
-std::size_t strlen(const char* chars);
 
 class String
 {
 public:
 	String();
 
-	String(const char* chars,std::size_t capacity);
+	String(const char* chars, std::size_t capacity);
 
-	String(const char* chars):String(chars,strlen(chars)){};
+	String(const char* chars);
 	
-	String(const String& str):String(str.c_str()){};
+	String(const String& str):String( str.c_str() ){}
 	
 	String operator=(String str);
 	
-	const bool empty()const {return mSize==0;}
+	const bool empty() const { return mSize==0; }
 
-	char& operator[](int idx)const { return mString[idx];}
+	const bool operator==(const String& str) const;
 
-	bool operator==(const String& str)const{return std::equal(c_str() , c_str() + mSize , str.c_str());};
+	char& operator[](const int idx) const { return mString[idx]; }
+
+	const char* c_str() const {return mString.get();}
+	
+	std::size_t size() const { return mSize; }
+	std::size_t capacity() const { return mCapacity; }
+
+	operator std::string() const { return c_str(); }
 
 	String& operator+(const String& str);
 	
-	operator std::string()const{return c_str();}
-
 	void push_back(const char c);
 	void add_capacity();
 
-	std::size_t size()const{return mSize;};
-	std::size_t capacity()const{return mCapacity;};
 
-	const char* c_str()const {return mString.get();}
 private:
-	std::unique_ptr<char[]> mString;
 	std::size_t mSize;
 	std::size_t mCapacity;
+	std::unique_ptr<char[]> mString;
 };
 
 
 bool operator==(const std::string& str1,const String& str2);
 
-bool operator==(const char* chars,const String& str);
+bool operator==(const char* chars, const String& str);
 
-String operator+(const char* , const String& str);
+String operator+(const char* chars, const String& str);
 
 std::ostream& operator<<(std::ostream&, const String&);

d7018e473dbc07877874b8ca2ed35496db84c75f
David Grs <david.a.grs@gmail.com>
2018-11-20 19:53:51 +0100

    course02: meeting00

--- course02/meeting00/main.cc
+++ course02/meeting00/main.cc
@@ -1,1 +1,18 @@
-int main(){}
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int i;
+int j = 5;
+
+int main()
+{
+	int k = 10;
+
+	std::cout << &i << std::endl;
+	std::cout << &j << std::endl;
+	std::cout << &k << std::endl;
+
+	std::this_thread::sleep_for(std::chrono::seconds(100));
+	return 0;
+}

bf3a2daf0bf15fe1adc6b6c6e1b0f76a1ec7739f
dovito <s1237357@sms.ed.ac.uk>
2018-12-10 19:58:12 +0100

    homework00

--- course02/homework00/skeleton/vector.h
+++ course02/homework00/skeleton/vector.h
@@ -1,8 +1,45 @@
 #pragma once
 
+#include <array>
+
 class VectorInt
 {
-	// TODO
+	
+public:
+	int32_t size()
+	{
+		return mSize;
+	}
+
+	int32_t capacity()
+	{
+		return mData.size();
+	}
+
+	int32_t back()
+	{
+		if (mSize > 0)		
+			return mData[mSize-1];
+	}
+
+	void push_back(int32_t member)
+	{
+
+		if (mSize < mData.size())
+		{
+			mData[mSize] = member;
+			++mSize;
+		}
+	}
+
+	void clear()
+	{
+		mSize = 0;
+	}
+
+private:
+	int32_t mSize{0};
+	std::array<int32_t, 128> mData;
 };
 
 

5efc32d30e2d566ef77e987104f9826214aeb488
elfieee <elfie_9999@yahoo.com>
2018-12-10 23:53:20 +0100

    yun's commit

--- course02/homework00/yun/vector.h
+++ course02/homework00/yun/vector.h
@@ -1,8 +1,25 @@
+#include <array>
 #pragma once
 
 class VectorInt
 {
-	// TODO
+public:
+	int size();
+
+	int capacity();
+
+	int back();
+
+	void push_back(int);
+	
+	void clear();
+
+	VectorInt();
+
+private:
+	int vectorSize;
+	
+	std::array<int, 42> vectorData;
 };
 
 

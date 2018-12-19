30652112c74ddcecb0beda1ee881336039dca9fe
Matthew Robinson <matthewrobinson@optiver.com (file://matthewrobinson@optiver.com/)>
2018-12-19 17:51:29 +0100

    restructuring to resemble last course

--- course02/homework00/skeleton/vector.h
+++ course02/homework00/skeleton/vector.h
@@ -1,20 +1,6 @@
 #pragma once
-#include <array>
-
 
 class VectorInt
 {
-public:
-	int capacity() const;
-	int size() const;
-	int back() const;
-	int& back();
-	void push_back(int) ;
-	void clear();	
-private:
-	static const int mMaxSize = 256;
-	std::array<int, mMaxSize> mData;
-	int mSize = 0;
+	// TODO
 };
-
-

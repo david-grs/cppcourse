f755f53631bfe889ec9ef781b52d8ed3051d7507
Joost Lek <vlabakje@gmail.com>
2018-12-19 20:17:03 +0100

    Style fixes

--- course02/homework00/joost/vector.h
+++ course02/homework00/joost/vector.h
@@ -4,44 +4,46 @@
 
 class VectorInt
 {
-	public:
-		VectorInt():
-			mSize(0)
-		{}
-		
-		int size() const
+public:
+	VectorInt():
+		mSize(0)
+	{}
+	
+	int size() const
+	{
+		return mSize;
+	}
+
+	int capacity() const
+	{
+		return mData.size();
+	}
+
+	void push_back(int value)
+	{
+		if( mSize == mData.size() )
 		{
-			return mSize;
+			throw std::out_of_range("vector at capacity");
 		}
+		mData[mSize++] = value;
+	}
 
-		int capacity() const
+	int back(){
+		if( mSize == 0 )
 		{
-			return mData.size();
+			throw std::out_of_range("vector is empty");
 		}
+		return mData.at(--mSize);
+	}
 
-		void push_back(int value)
-		{
-			if(mSize == mData.size()){
-				throw std::out_of_range("vector at capacity");
-			}
-			mData[mSize++] = value;
-		}
+	void clear(){
+		// no need to overwrite data
+		mSize = 0;
+	}
 
-		int back(){
-			if(mSize == 0){
-				throw std::out_of_range("vector is empty");
-			}
-			return mData.at(--mSize);
-		}
-
-		void clear(){
-			// no need to overwrite data
-			mSize = 0;
-		}
-
-	private:
-		std::array<int, 10> mData;
-		std::size_t mSize; // how many items are in the vector
+private:
+	std::array<int, 10> mData;
+	std::size_t mSize; // how many items are in the vector
 };
 
 

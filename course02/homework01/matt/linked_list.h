d214da83f80191fce276738a748296786bf5e42c
Ma Ro <robbo243@gmail.com>
2019-01-08 22:48:04 +0100

    changing from shared_ptr to unique_ptr moving some implementation details to .cc file other minor PR changes

--- course02/homework01/matt/linked_list.h
+++ course02/homework01/matt/linked_list.h
@@ -1,15 +1,14 @@
 #include <memory>
-#include <iostream>
 
 template<typename T>
 struct Node
 {
-    Node(const T& t)
-    : mData(t)
-    {};
+	Node(const T& t)
+	: mData(t)
+	{};
 
-    T mData;
-    std::shared_ptr<Node<T>> mNext;
+	T mData;
+	std::unique_ptr<Node<T>> mNext;
 };
 
 template<typename T>
@@ -17,77 +16,30 @@ class LinkedList
 {
 public:
 
-    using PtrNode = std::shared_ptr<Node<T>>;
+	LinkedList();
+	
+	LinkedList(const LinkedList& other);
+	
+	LinkedList& operator=(LinkedList other);
 
-    LinkedList() {};
-    
-    LinkedList(const LinkedList& other)
-    {
-        std::cout << "copy constructor" << std::endl;
-        
-        if (other.mSize == 0)
-            return;
-        
-        PtrNode ptrNode = other.mFirst;
-        while(ptrNode != nullptr)
-            {
-                Append(ptrNode->mData);
-                ptrNode = ptrNode->mNext;
-            }
-    };
+	void Append(const T& data);
 
-    LinkedList& operator=(const LinkedList& other)
-    {
-        std::cout << "assignment operator" << std::endl;
-
-        mFirst.reset();
-        mSize = 0;
-
-        if (other.mSize == 0)
-            return *this;
-        
-        PtrNode ptrNode = other.mFirst;
-        while(ptrNode != nullptr)
-            {
-                Append(ptrNode->mData);
-                ptrNode = ptrNode->mNext;
-            }
-        return *this;
-    }
-
-    void Append(const T& data)
-    {
-        if(mSize == 0)
-        {
-            mFirst = std::make_shared<Node<T>>(data);
-        }
-        else
-        {
-            PtrNode ptrNode = mFirst;
-            while(ptrNode->mNext != nullptr)
-            {
-                ptrNode = ptrNode->mNext;
-            }
-            ptrNode->mNext =  std::make_shared<Node<T>>(data);
-        }
-
-        mSize++;
-    }
-
-    friend std::ostream& operator<<(std::ostream& stream, const LinkedList& list)
-    {
-        PtrNode ptrNode = list.mFirst;
-        
-        while(ptrNode != nullptr)
-        {
-            stream << ptrNode->mData << ", ";
-            ptrNode = ptrNode->mNext;
-        }
-
-        return stream;
-    }
+	friend std::ostream& operator<<(std::ostream& stream, const LinkedList& list)
+	{
+	    Node<T>* ptrNode = list.mFirst.get();
+	    while(ptrNode != nullptr)
+	    {
+	        stream << ptrNode->mData << ", ";
+	        ptrNode = ptrNode->mNext.get();
+	    }
+	    
+		return stream;
+	}
 
 private:
-    PtrNode mFirst;
-    int mSize {};
+	std::unique_ptr<Node<T>> mFirst;
+	Node<T>* mLast;
+	int mSize {};
 };
+
+#include "linked_list.cc"
--- course02/homework01/matt/test_linked_list.cc
+++ course02/homework01/matt/test_linked_list.cc
@@ -3,20 +3,20 @@
 
 int main()
 {
-    LinkedList<int> l;
-    l.Append(1);
-    l.Append(2);
-    l.Append(3);
+	LinkedList<int> l;
+	l.Append(1);
+	l.Append(2);
+	l.Append(3);
 
-    std::cout << "l:" << l << std::endl;
+	std::cout << "l:" << l << std::endl;
 
-    LinkedList<int> lCopyConstructed(l);
-    std::cout << "lCopyConstructed:" << lCopyConstructed << std::endl;
-    
-    LinkedList<int> lAssigned;
-    lAssigned = l;
-    std::cout << "lAssigned:" << lAssigned << std::endl;
-    
-    return 0;
+	LinkedList<int> lCopyConstructed(l);
+	std::cout << "lCopyConstructed:" << lCopyConstructed << std::endl;
+	
+	LinkedList<int> lAssigned;
+	lAssigned = l;
+	std::cout << "lAssigned:" << lAssigned << std::endl;
+
+	return 0;
 }
 

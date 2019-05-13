aa57bbe999b6e381b9369282c83499f70bffe1f3
balint <borbalint@gmail.com>
2019-05-13 21:59:19 +0200

    some progress with the LinkedListInt

--- course03/homework01/balint/main.cpp
+++ course03/homework01/balint/main.cpp
@@ -1,43 +1,69 @@
 #include <iostream>
-class LinkedListInt{
+#include <memory>
+
+class LinkedListInt {
 public:
-  bool isEmpty() const {
-    std::cout<<"element is: "<<elem<<std::endl;
-    return elem ? false : true;
-  }
-  LinkedListInt static emptyList()  {
+  bool isEmpty() const { return elem ? false : true; }
+  LinkedListInt static emptyList() {
     LinkedListInt retval;
-    return retval;};
+    return retval;
+  };
 
-  LinkedListInt static cons(LinkedListInt, int)  {
-    LinkedListInt retval;
-    return retval;};
-  
-  // LinkedListInt(int i, LinkedListInt l): elem(std::make_unique<int>(i)),next(std::make_unique<LinkedListInt>(l)) {};
-  LinkedListInt(int i, LinkedListInt l): elem(std::make_unique<int>(i)),next(std::make_unique<LinkedListInt>(emptyList())) {};
+  LinkedListInt(int i, LinkedListInt &l) {
+    elem = std::make_unique<int>(i);
+    if (!*(l.next)) {
+      next = std::make_unique<LinkedListInt>();
+    } else {
+      next = std::make_unique<LinkedListInt>(*(l.elem), *(l.next));
+    }
+  };
 
-  LinkedListInt(int i): elem(std::make_unique<int>(i)),next(std::make_unique<LinkedListInt>()) {
-    std::cout<<"element is: "<<i<<std::endl;};
+  LinkedListInt(int i) {
+    elem = std::make_unique<int>(i);
+    next = std::make_unique<LinkedListInt>();
+  };
 
-  LinkedListInt(): elem(),next() {};
+  LinkedListInt() : elem(), next(){};
+  std::unique_ptr<int> car(){return std::make_unique<int>(*elem);}
+
+  operator bool() const {return isEmpty();}
+
 private:
   std::unique_ptr<int> elem;
   std::unique_ptr<LinkedListInt> next;
-
 };
 
-int main(){
-  LinkedListInt test =LinkedListInt();
-  if (test.isEmpty()){std::cout<<"test is empty"<<std::endl;}
-  if (!test.isEmpty()){std::cout<<"test is NOT empty"<<std::endl;}
+LinkedListInt static cons(int i, LinkedListInt &l) {
+  LinkedListInt retval = LinkedListInt(i, l);
+  return retval;
+};
 
-  LinkedListInt test2 =3;
-  if (test2.isEmpty()){std::cout<<"test2 is empty"<<std::endl;}
-  if (!test2.isEmpty()){std::cout<<"test2 is NOT empty"<<std::endl;}
+std::unique_ptr<int> car(LinkedListInt l){return l.car();}
 
-  LinkedListInt test3 ={4, test2};
-  if (test3.isEmpty()){std::cout<<"test3 is empty"<<std::endl;}
-  if (!test3.isEmpty()){std::cout<<"test3 is NOT empty"<<std::endl;}
+int main() {
+  LinkedListInt test = LinkedListInt();
+  if (test.isEmpty()) {
+    std::cout << "test is empty" << std::endl;
+  }
+  if (!test.isEmpty()) {
+    std::cout << "test is NOT empty" << std::endl;
+  }
+
+  LinkedListInt test2 = 3;
+  if (test2.isEmpty()) {
+    std::cout << "test2 is empty" << std::endl;
+  }
+  if (!test2.isEmpty()) {
+    std::cout << "test2 is NOT empty" << std::endl;
+  }
+
+  LinkedListInt test3 = {4, test2};
+  if (test3.isEmpty()) {
+    std::cout << "test3 is empty" << std::endl;
+  }
+  if (!test3) {
+    std::cout << "test3 is NOT empty" << std::endl;
+  }
 
   return 0;
 }

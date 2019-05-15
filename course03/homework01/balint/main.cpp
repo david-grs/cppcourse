5ee1c582ba2ced9f3002407f8bbddc79b04cce95
balint <borbalint@gmail.com>
2019-05-15 22:32:02 +0200

    cleanup, copy/= constructor, destructor

--- course03/homework01/balint/main.cpp
+++ course03/homework01/balint/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <memory>
 
@@ -11,10 +12,10 @@ public:
 
   LinkedListInt(int i, LinkedListInt &l) {
     elem = std::make_unique<int>(i);
-    if (!*(l.next)) {
-      next = std::make_unique<LinkedListInt>();
-    } else {
+    if (l) {
       next = std::make_unique<LinkedListInt>(*(l.elem), *(l.next));
+    } else {
+      next = std::make_unique<LinkedListInt>();
     }
   };
 
@@ -24,22 +25,69 @@ public:
   };
 
   LinkedListInt() : elem(), next(){};
-  std::unique_ptr<int> car(){return std::make_unique<int>(*elem);}
+  std::unique_ptr<int> car() { return std::make_unique<int>(*elem); }
 
-  operator bool() const {return isEmpty();}
+  operator bool() const { return isEmpty(); }
+
+  LinkedListInt cad() {
+    if (isEmpty()) {
+      return emptyList();
+    }
+    if (next->isEmpty()) {
+      return emptyList();
+    } else {
+      // return LinkedListInt(*this->car(), *((*(next)).next));
+      return LinkedListInt(*next->car(), *((*(next)).next));
+    }
+  }
+
+  LinkedListInt &operator=(const LinkedListInt &other) {
+
+    if (other) {
+      auto newelem = std::make_unique<int>(*other.elem);
+      elem = std::move(newelem);
+      if (*(other.next)) {
+        auto newnext = std::make_unique<LinkedListInt>(*(*other.next).elem,
+                                                       *(*other.next).next);
+        next = std::move(newnext);
+      }
+    }
+    return *this;
+  }
+  LinkedListInt(const LinkedListInt &other) {
+    if (other) {
+      // LinkedListInt(*(other.elem), *(other.next))
+      std::unique_ptr<int> newelem = std::make_unique<int>(*other.elem);
+      elem = std::move(newelem);
+
+      if (*(other.next)) {
+        auto newnext = std::make_unique<LinkedListInt>(*(*other.next).elem,
+                                                       *(*other.next).next);
+        next = std::move(newnext);
+      } else {
+        auto newnext = std::make_unique<LinkedListInt>();
+        next = std::move(newnext);
+      }
+    }
+  }
+  ~LinkedListInt(){
+    elem.reset();
+    next.reset();
+  }
 
 private:
   std::unique_ptr<int> elem;
   std::unique_ptr<LinkedListInt> next;
 };
 
-LinkedListInt static cons(int i, LinkedListInt &l) {
+LinkedListInt cons(int i, LinkedListInt &l) {
   LinkedListInt retval = LinkedListInt(i, l);
   return retval;
 };
 
-std::unique_ptr<int> car(LinkedListInt l){return l.car();}
+std::unique_ptr<int> car(LinkedListInt l) { return l.car(); }
 
+LinkedListInt cad(LinkedListInt &l) { return l.cad(); }
 int main() {
   LinkedListInt test = LinkedListInt();
   if (test.isEmpty()) {
@@ -58,11 +106,22 @@ int main() {
   }
 
   LinkedListInt test3 = {4, test2};
+  std::cout << "test3 is empty" << std::endl;
+
   if (test3.isEmpty()) {
     std::cout << "test3 is empty" << std::endl;
   }
   if (!test3) {
     std::cout << "test3 is NOT empty" << std::endl;
+  }
+
+  // LinkedListInt test4 = cad(test3);
+  LinkedListInt test4 = test3.cad();
+  if (test4.isEmpty()) {
+    std::cout << "test4 is empty" << std::endl;
+  }
+  if (!test4.isEmpty()) {
+    std::cout << "test4 is NOT empty" << std::endl;
   }
 
   return 0;

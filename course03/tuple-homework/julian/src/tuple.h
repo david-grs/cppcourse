f40e50c50f6989ba7ebcb607df4fd051407f2319
jundl77 <julianbrendl@gmail.com>
2019-07-07 21:36:15 +0200

    very basic tuple implementation

---
+++ course03/tuple-homework/julian/src/tuple.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string>
+#include <boost/optional.hpp>
+#include <iostream>
+
+namespace CppCourse { namespace TupleHomework {
+
+template<class Head, class... Args>
+class Tuple : public Tuple<Args...>
+{
+public:
+	Tuple(Head head, Args... args)
+	:   Tuple<Args...>(args...),
+		mArgs(Tuple<Args...>(args...)),
+		mHead(head)
+	{
+	}
+
+	int Size()
+	{
+		return SizeHelper(mHead, mArgs);
+	}
+
+	Head Get(int index)
+	{
+		if (index == 0)
+		{
+			return mHead;
+		}
+
+		return mArgs.Get(--index);
+	}
+
+private:
+	int SizeHelper(Head head, Tuple<Args...> tuple)
+	{
+		return 1 + tuple.Size();
+	}
+
+private:
+	Head mHead;
+	Tuple<Args...> mArgs;
+};
+
+template<class Head>
+class Tuple<Head> {
+public:
+	Tuple(Head head) : mHead(head) {}
+
+	int Size()
+	{
+		return 1;
+	}
+
+	Head Get(int index)
+	{
+		return mHead;
+	}
+
+private:
+	Head mHead;
+};
+
+}};

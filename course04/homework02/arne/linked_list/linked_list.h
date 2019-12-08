2fceea521a1c63301879c1313050c8d956996fde
donald <donald@duck.com>
2019-12-08 13:37:16 +0100

    linked list template

--- course04/homework02/arne/linked_list/linked_list.h
+++ course04/homework02/arne/linked_list/linked_list.h
@@ -1,18 +1,61 @@
 #pragma once
 #include <memory>
 
+template <class T>
 struct Node
 {
-    Node(int data) : mData{data} {}
-    int mData;
+    Node(T data) : mData{data} {}
+    T mData;
     std::unique_ptr<Node> mNextNode = nullptr;
 };
 
+template <class T>
 struct LinkedList
 {
     LinkedList() = default;
     bool IsEmpty() { return mNode == nullptr; }
-    void Append(int data);
-    void Remove(int data);
-    std::unique_ptr<Node> mNode = nullptr;
+    void Append(T data)
+    {
+        Node<T>* searchEmptyNodePtr = this->mNode.get();
+
+        if (searchEmptyNodePtr == nullptr)
+        {
+            this->mNode = std::make_unique<Node<T>>(data);
+            return;
+        }
+
+        while (searchEmptyNodePtr->mNextNode != nullptr)
+        {
+            searchEmptyNodePtr = searchEmptyNodePtr->mNextNode.get();
+        }
+
+        searchEmptyNodePtr->mNextNode = std::make_unique<Node<T>>(data);
+    }
+    void Remove(T data)
+    {
+        if (this->IsEmpty())
+            return;
+
+        Node<T>* nodeptr1 = this->mNode.get();
+
+        if (data == nodeptr1->mData)
+        {
+            this->mNode = move(nodeptr1->mNextNode);
+            return;
+        }
+
+        Node<T>* nodeptr2 = nodeptr1->mNextNode.get();
+        while (nodeptr2 != nullptr)
+        {
+            if (data == nodeptr2->mData)
+            {
+                nodeptr1->mNextNode = move(nodeptr2->mNextNode);
+                return;
+            }
+
+            nodeptr1 = nodeptr2;
+            nodeptr2 = nodeptr2->mNextNode.get();
+        }
+    }
+    std::unique_ptr<Node<T>> mNode = nullptr;
 };
--- course04/homework02/arne/tests/linked_list_test.cc
+++ course04/homework02/arne/tests/linked_list_test.cc
@@ -3,56 +3,100 @@
 
 struct linked_list_tests : public ::testing::Test
 {
-    LinkedList linkedList;
+    LinkedList<int> linkedListInt;
+    LinkedList<char> linkedListChar;
 
-    void ExpectEmptyList()
+    void ExpectEmptyListInt()
     {
-        EXPECT_TRUE(&linkedList);
-        EXPECT_TRUE(linkedList.IsEmpty());
-        EXPECT_EQ(nullptr, linkedList.mNode);
+        EXPECT_TRUE(&linkedListInt);
+        EXPECT_TRUE(linkedListInt.IsEmpty());
+        EXPECT_EQ(nullptr, linkedListInt.mNode);
+    }
+
+    void ExpectEmptyListChar()
+    {
+        EXPECT_TRUE(&linkedListChar);
+        EXPECT_TRUE(linkedListChar.IsEmpty());
+        EXPECT_EQ(nullptr, linkedListChar.mNode);
     }
 };
 
-
 TEST_F(linked_list_tests, default_ctor)
 {
-    ExpectEmptyList();
+    ExpectEmptyListInt();
+    ExpectEmptyListChar();
 }
 
-TEST_F(linked_list_tests, append_single_to_list)
+TEST_F(linked_list_tests, append_single_to_list_int)
 {
-    linkedList.Append(1);
-    EXPECT_FALSE(linkedList.IsEmpty());
-    EXPECT_EQ(1, linkedList.mNode->mData);
+    linkedListInt.Append(1);
+    EXPECT_FALSE(linkedListInt.IsEmpty());
+    EXPECT_EQ(1, linkedListInt.mNode->mData);
 }
 
-TEST_F(linked_list_tests, append_multiple_to_list)
+TEST_F(linked_list_tests, append_single_to_list_char)
 {
-    linkedList.Append(1);
-    linkedList.Append(7);
-    EXPECT_FALSE(linkedList.IsEmpty());
-    EXPECT_EQ(1, linkedList.mNode->mData);
-    EXPECT_EQ(7, linkedList.mNode->mNextNode->mData);
+    linkedListChar.Append('1');
+    EXPECT_FALSE(linkedListChar.IsEmpty());
+    EXPECT_EQ('1', linkedListChar.mNode->mData);
 }
 
-TEST_F(linked_list_tests, delete_from_list)
+TEST_F(linked_list_tests, append_multiple_to_list_int)
 {
-    linkedList.Append(1);
-    linkedList.Append(7);
-    linkedList.Append(9);
-    linkedList.Remove(9);
-    linkedList.Remove(1);
-    linkedList.Remove(7);
-
-    ExpectEmptyList();
+    linkedListInt.Append(1);
+    linkedListInt.Append(7);
+    EXPECT_FALSE(linkedListInt.IsEmpty());
+    EXPECT_EQ(1, linkedListInt.mNode->mData);
+    EXPECT_EQ(7, linkedListInt.mNode->mNextNode->mData);
 }
 
-TEST_F(linked_list_tests, dont_delete_from_list)
+TEST_F(linked_list_tests, append_multiple_to_list_char)
 {
-    linkedList.Append(1);
-    linkedList.Remove(7);
-    EXPECT_FALSE(linkedList.IsEmpty());
-    EXPECT_EQ(1, linkedList.mNode->mData);
+    linkedListChar.Append('1');
+    linkedListChar.Append('7');
+    EXPECT_FALSE(linkedListChar.IsEmpty());
+    EXPECT_EQ('1', linkedListChar.mNode->mData);
+    EXPECT_EQ('7', linkedListChar.mNode->mNextNode->mData);
+}
+
+TEST_F(linked_list_tests, delete_from_list_int)
+{
+    linkedListInt.Append(1);
+    linkedListInt.Append(7);
+    linkedListInt.Append(9);
+    linkedListInt.Remove(9);
+    linkedListInt.Remove(1);
+    linkedListInt.Remove(7);
+
+    ExpectEmptyListInt();
+}
+
+TEST_F(linked_list_tests, delete_from_list_char)
+{
+    linkedListChar.Append('1');
+    linkedListChar.Append('7');
+    linkedListChar.Append('9');
+    linkedListChar.Remove('9');
+    linkedListChar.Remove('1');
+    linkedListChar.Remove('7');
+
+    ExpectEmptyListChar();
+}
+
+TEST_F(linked_list_tests, dont_delete_from_list_int)
+{
+    linkedListInt.Append(1);
+    linkedListInt.Remove(7);
+    EXPECT_FALSE(linkedListInt.IsEmpty());
+    EXPECT_EQ(1, linkedListInt.mNode->mData);
+}
+
+TEST_F(linked_list_tests, dont_delete_from_list_char)
+{
+    linkedListChar.Append('1');
+    linkedListChar.Remove('7');
+    EXPECT_FALSE(linkedListChar.IsEmpty());
+    EXPECT_EQ('1', linkedListChar.mNode->mData);
 }
 
 int main(int argc, char **argv)

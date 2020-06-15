aa900e2cfdfa9283d0b232b3fdddd8a1e3d5fd55
Joel Christiansen <joelchristiansen@Optiver.com>
2020-06-15 17:20:49 +0200

    copy skeleton to joel

---
+++ course05/homework01/joel/string_tests.cc
@@ -0,0 +1,22 @@
+#include "string.h"
+
+#include <cassert>
+
+void TestEmpty()
+{
+	String str;
+	assert(str.Empty());
+}
+
+int main()
+{
+#ifdef NDEBUG
+#error "This file needs to be built with Debug flags"
+#endif
+
+	TestEmpty();
+
+	// TODO: other tests
+
+	return 0;
+}

31f0a6562fca274e0b8da2ff469cc6964907ed03
Joel Christiansen <joelchristiansen@Optiver.com>
2020-06-16 16:53:02 +0200

    adding constructors, operators, starts/endswith, empty/size/c_str, tests for all the above

--- course05/homework01/joel/string.h
+++ course05/homework01/joel/string.h
@@ -1,23 +1,66 @@
 #pragma once
 
-#include <string>
+#include <cstring>
+#include <iostream>
+#include <memory>
 #include <ostream>
+#include <string>
 
 class String
 {
 public:
-	String();
-	String(std::string);
+	String(const char s[]) : mSize(std::strlen(s)), mContents(std::make_unique<char[]>(mSize))
+	{
+		std::strcpy(mContents.get(), s);
+	}
+	String() : String("") {}
+	String(std::string s) : String(s.c_str()) {}
+	String(const String &s) : String(s.c_str()) {}
 
-	bool Empty() const;
-	std::size_t Size() const;
+	bool Empty() const { return mSize == 0; };
+	std::size_t Size() const { return mSize; };
+	const char *c_str() const { return mContents.get(); };
 
-	// TODO
+	// mainly added these two to help with testing operator+
+	bool StartsWith(String substr) const
+	{
+		return (std::strncmp(c_str(), substr.c_str(), substr.Size()) == 0);
+	}
+
+	bool EndsWith(String substr) const
+	{
+		// First, find the substring
+		const char *found_sub = std::strstr(c_str(), substr.c_str());
+		if (found_sub == NULL)
+		{
+			// If it's not there, we're done
+			return false;
+		}
+		// else, compare the found sub to the given one.
+		// do not use strncmp here, because if the found substr is longer than the given one it should be
+		// false
+		return std::strcmp(found_sub, substr.c_str()) == 0;
+	}
+
+private:
+	std::size_t mSize;
+	std::unique_ptr<char[]> mContents;
 };
 
-std::ostream& operator<<(std::ostream& stream, String str)
+std::ostream &operator<<(std::ostream &stream, String &str)
 {
-	// TODO
+	stream << str.c_str();
 	return stream;
 }
 
+bool operator==(String &str1, String &str2) { return std::strcmp(str1.c_str(), str2.c_str()) == 0; }
+
+bool operator!=(String &str1, String &str2) { return !(str1 == str2); }
+
+String operator+(String &str1, String &str2)
+{
+	char tmpstr[str1.Size() + str2.Size()];
+	std::strcpy(tmpstr, str1.c_str());
+	std::strcat(tmpstr, str2.c_str());
+	return String(tmpstr);
+}
--- course05/homework01/joel/string_tests.cc
+++ course05/homework01/joel/string_tests.cc
@@ -1,11 +1,60 @@
 #include "string.h"
 
 #include <cassert>
+#include <cstring>
+#include <iostream>
+#include <sstream>
 
 void TestEmpty()
 {
 	String str;
 	assert(str.Empty());
+	assert(std::strcmp(str.c_str(), "") == 0);
+}
+
+void TestContents()
+{
+	char base[] = "this is a string";
+	String str{base};
+	assert(std::strcmp(str.c_str(), base) == 0);
+}
+
+void TestStream()
+{
+	std::stringstream stream;
+	char base[] = "This is a string";
+	String str{base};
+	stream << str;
+	assert(stream.str() == base);
+}
+
+void TestOperators()
+{
+	// few example strings
+	String str1{"This is a string"};
+	String str2{str1};
+	String str3{"This is a different string"};
+	// eq and ne
+	assert(str1 == str2);
+	assert(str1 != str3);
+
+	// +
+	String addedstr{str1 + str3};
+	assert(addedstr.StartsWith(str1));
+	// Missing letter at the start
+	assert(!addedstr.StartsWith("his is a string"));
+	// Extra letter at the start
+	assert(!addedstr.StartsWith("TThis is a string"));
+	// Extra letter at the end
+	assert(!addedstr.StartsWith("This is a stringg"));
+	assert(addedstr.EndsWith(str3));
+	// extra letter at the end
+	assert(!addedstr.EndsWith("This is a different stringg"));
+	// missing letter at the end
+	assert(!addedstr.EndsWith("This is a different strin"));
+	// extra letter at the start
+	assert(!addedstr.EndsWith("TThis is a different string"));
+
 }
 
 int main()
@@ -15,6 +64,9 @@ int main()
 #endif
 
 	TestEmpty();
+	TestContents();
+	TestStream();
+	TestOperators();
 
 	// TODO: other tests
 

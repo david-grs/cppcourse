cd4b2392f1ee4b6e4c033a9536ff428bebd95585
Harro van der Kroft <wisepotato@users.noreply.github.com>
2020-06-24 20:34:30 +0200

    doesnt build

--- course05/homework01/harrovanderkroft/string.h
+++ course05/homework01/harrovanderkroft/string.h
@@ -43,7 +43,7 @@ public:
         if(totalLength > MAX_STRING_SIZE){
             throw std::invalid_argument("Can't concatenate more than 1024 in total");
         }
-        int indexStart = this->length();
+        int indexStart = (int)this->Size();
         for(int i = 0; i <= indexStart; i++){
             this->chars[indexStart + i] = other.CharAt(i);
         }
@@ -55,35 +55,33 @@ public:
         return !(this == &other); 
     }
 
-    bool operator==(String other) {
-        if(this->length() != other.length()){
+    bool operator==(String* other) {
+        if(this->Size() != other->Size()){
             return false;
         }
-        for(int i =0; i < this->length(); i++){
-            // could not get this to work,
-            //  i wanted this[i]; hence me using
-            //. the [] operator overload
-            char actualChar = this->chars[i];
-            if(actualChar !=  other[i]){
+        for(int i = 0; i < this->Size(); i++){
+            if(this->at(i) != other->at(i)){
                 return false;
             }
         }
         return true;
     }
 
-    char operator [](int i) const    {
+    char at(int i) const {
         return this->chars[i];
     }
-    
-    int length() {
-        return this->size_of_string;
+
+    char operator[](int i) const    {
+        return this->at(i);
     }
+    
+    std::size_t Size() const {
+        return (std::size_t) this->size_of_string;
+    }
+
 	bool Empty() const {
         return true;
     }
-	std::size_t Size() const;
-
-	// TODO
 };
 
 std::ostream& operator<<(std::ostream& stream, String str)

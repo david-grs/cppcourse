cf6befa5b942ccd7bfb6c6da728b6a4235e04ff2
samhaywood <samjhay@hotmail.co.uk>
2020-06-26 18:37:00 +0200

    Initial commit

--- course05/homework01/Sam/string.h
+++ course05/homework01/Sam/string.h
@@ -9,10 +9,14 @@ public:
     String();
     String(std::string);
 
-    bool Empty() const;
+    bool Empty() const {return mSize==0;};
     std::size_t Size() const;
 
     // TODO
+private:
+    size_t mSize;
+    int mCapacity;
+    
 };
 
 std::ostream& operator<<(std::ostream& stream, String str)
